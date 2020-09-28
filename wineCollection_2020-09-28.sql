1da618c113189e5243446f761f2cfddc506a64a2
Ellie <erobinson98@btinternet.com>
2020-09-28 13:54:55 +0100

    restructure db to get rid of cost table

--- wineCollection_2020-09-28.sql
+++ wineCollection_2020-09-28.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.31)
 # Database: wineCollection
-# Generation Time: 2020-09-28 12:22:49 +0000
+# Generation Time: 2020-09-28 12:54:30 +0000
 # ************************************************************
 
 
@@ -47,31 +47,6 @@ VALUES
 UNLOCK TABLES;
 
 
-# Dump of table cost
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `cost`;
-
-CREATE TABLE `cost` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `cost` float NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-LOCK TABLES `cost` WRITE;
-/*!40000 ALTER TABLE `cost` DISABLE KEYS */;
-
-INSERT INTO `cost` (`id`, `cost`)
-VALUES
-	(1,7.99),
-	(2,15.99),
-	(3,8),
-	(4,7.5);
-
-/*!40000 ALTER TABLE `cost` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
 # Dump of table country
 # ------------------------------------------------------------
 
@@ -107,7 +82,7 @@ CREATE TABLE `wine` (
   `variety` varchar(100) NOT NULL DEFAULT '',
   `tones` text NOT NULL,
   `brand_id` int(11) NOT NULL,
-  `cost_id` int(11) NOT NULL,
+  `cost` float NOT NULL,
   `country_of_origin` int(11) NOT NULL,
   `img_location` text NOT NULL,
   PRIMARY KEY (`id`)
@@ -116,15 +91,15 @@ CREATE TABLE `wine` (
 LOCK TABLES `wine` WRITE;
 /*!40000 ALTER TABLE `wine` DISABLE KEYS */;
 
-INSERT INTO `wine` (`id`, `variety`, `tones`, `brand_id`, `cost_id`, `country_of_origin`, `img_location`)
+INSERT INTO `wine` (`id`, `variety`, `tones`, `brand_id`, `cost`, `country_of_origin`, `img_location`)
 VALUES
-	(1,'Cabernet Sauvignon','Concoction of ripe red berries, toasted oak, rich vanilla, mocha and black pepper.',1,2,1,'imgs/cab-sav.png'),
-	(2,'Shiraz (Yellow Label)',' Plum, spice and pepper characters with subtle oak.',2,3,1,'imgs/shiraz.jpeg'),
-	(3,'Merlot (Black Label)','Ripe fruit flavours of raspberry, cherry and plums integrated with subtle oak',3,3,1,'imgs/merlot.jpg'),
-	(4,'Rioja','Intense rich flavours of ripe red cherries and strawberries, followed by sweet vanilla and spice.',4,3,2,'imgs/rioja.jpeg'),
-	(5,'Pinotage','Ripe flavours of raspberry, plum and black cherry, finishing with a hint of oak.',5,4,3,'imgs/pinotage.jpg'),
-	(6,'Pinot Noir','Juicy red fruit (think cranberries, cherries, and raspberries), and delicate hints of cedar, smoke, and bay leaf.',1,2,1,'imgs/pinot-noir.png'),
-	(7,'Malbec','Juicy plum, redcurrant and blackberry fruit flavours are notes of chocolate, bay leaves and spice, toasted with a little oak that rounds off the palate.',6,1,4,'imgs/malbec.jpeg');
+	(1,'Cabernet Sauvignon','Concoction of ripe red berries, toasted oak, rich vanilla, mocha and black pepper.',1,15.99,1,'imgs/cab-sav.png'),
+	(2,'Shiraz (Yellow Label)',' Plum, spice and pepper characters with subtle oak.',2,8,1,'imgs/shiraz.jpeg'),
+	(3,'Merlot (Black Label)','Ripe fruit flavours of raspberry, cherry and plums integrated with subtle oak',3,8,1,'imgs/merlot.jpg'),
+	(4,'Rioja','Intense rich flavours of ripe red cherries and strawberries, followed by sweet vanilla and spice.',4,8,2,'imgs/rioja.jpeg'),
+	(5,'Pinotage','Ripe flavours of raspberry, plum and black cherry, finishing with a hint of oak.',5,7.5,3,'imgs/pinotage.jpg'),
+	(6,'Pinot Noir','Juicy red fruit (think cranberries, cherries, and raspberries), and delicate hints of cedar, smoke, and bay leaf.',1,15.99,1,'imgs/pinot-noir.png'),
+	(7,'Malbec','Juicy plum, redcurrant and blackberry fruit flavours are notes of chocolate, bay leaves and spice, toasted with a little oak that rounds off the palate.',6,7.99,4,'imgs/malbec.jpeg');
 
 /*!40000 ALTER TABLE `wine` ENABLE KEYS */;
 UNLOCK TABLES;
