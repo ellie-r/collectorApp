d69e36772df9c18b21f105f1cc18926b64537dac
Ellie <erobinson98@btinternet.com>
2020-09-28 21:41:18 +0100

    add items to db

--- wineCollection_2020-09-28.sql
+++ wineCollection_2020-09-28.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.31)
 # Database: wineCollection
-# Generation Time: 2020-09-28 12:54:30 +0000
+# Generation Time: 2020-09-28 20:40:58 +0000
 # ************************************************************
 
 
@@ -41,7 +41,9 @@ VALUES
 	(3,'McGuigan'),
 	(4,'Campo Viejo'),
 	(5,'Sainsbury'),
-	(6,'Waitrose');
+	(6,'Waitrose'),
+	(7,'Laughing Llama'),
+	(8,'Yellow Tail');
 
 /*!40000 ALTER TABLE `brands` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -66,7 +68,8 @@ VALUES
 	(1,'Australia'),
 	(2,'Spain'),
 	(3,'South Africa'),
-	(4,'Argentina');
+	(4,'Argentina'),
+	(5,'Chile');
 
 /*!40000 ALTER TABLE `country` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -94,12 +97,14 @@ LOCK TABLES `wine` WRITE;
 INSERT INTO `wine` (`id`, `variety`, `tones`, `brand_id`, `cost`, `country_of_origin`, `img_location`)
 VALUES
 	(1,'Cabernet Sauvignon','Concoction of ripe red berries, toasted oak, rich vanilla, mocha and black pepper.',1,15.99,1,'imgs/cab-sav.png'),
-	(2,'Shiraz (Yellow Label)',' Plum, spice and pepper characters with subtle oak.',2,8,1,'imgs/shiraz.jpeg'),
-	(3,'Merlot (Black Label)','Ripe fruit flavours of raspberry, cherry and plums integrated with subtle oak',3,8,1,'imgs/merlot.jpg'),
-	(4,'Rioja','Intense rich flavours of ripe red cherries and strawberries, followed by sweet vanilla and spice.',4,8,2,'imgs/rioja.jpeg'),
-	(5,'Pinotage','Ripe flavours of raspberry, plum and black cherry, finishing with a hint of oak.',5,7.5,3,'imgs/pinotage.jpg'),
+	(2,'Shiraz (Yellow Label)',' Plum, spice and pepper characters with subtle oak.',2,8,1,'imgs/shiraz.png'),
+	(3,'Merlot (Black Label)','Ripe fruit flavours of raspberry, cherry and plums integrated with subtle oak',3,8,1,'imgs/merlot.png'),
+	(4,'Rioja','Intense rich flavours of ripe red cherries and strawberries, followed by sweet vanilla and spice.',4,8,2,'imgs/rioja.png'),
+	(5,'Pinotage','Ripe flavours of raspberry, plum and black cherry, finishing with a hint of oak.',5,7.5,3,'imgs/pinotage.png'),
 	(6,'Pinot Noir','Juicy red fruit (think cranberries, cherries, and raspberries), and delicate hints of cedar, smoke, and bay leaf.',1,15.99,1,'imgs/pinot-noir.png'),
-	(7,'Malbec','Juicy plum, redcurrant and blackberry fruit flavours are notes of chocolate, bay leaves and spice, toasted with a little oak that rounds off the palate.',6,7.99,4,'imgs/malbec.jpeg');
+	(7,'Malbec','Juicy plum, redcurrant and blackberry, notes of chocolate, bay leaves and spice, toasted with a little oak that rounds off the palate.',6,7.99,4,'imgs/malbec.png'),
+	(8,'Merlot','Brimming with flavours of red fruits, warming spices and mocha.',7,5,5,'imgs/laughing-llama.png'),
+	(9,'Jammy Red Roo','Sweet and vibrant, with notes of juicy red berries, vanilla and chocolate.',8,7,1,'imgs/jammy-red-roo.png');
 
 /*!40000 ALTER TABLE `wine` ENABLE KEYS */;
 UNLOCK TABLES;
